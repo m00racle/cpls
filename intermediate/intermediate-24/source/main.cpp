2ad91df5c667e20ef4750a740a33603a01739f7e
m00racle <yanuar.prakosa@gmail.com>
2022-09-12 07:56:45 +0700

    add intermediate 24 Dynamic Memory

---
+++ intermediate/intermediate-24/source/main.cpp
@@ -0,0 +1,11 @@
+/* INTERMEDIATE 24 DYMANIC MEMORY
+THIS code is to demonstrate the practical pricnciples and Guidelines of using Dynamic Memory
+
+In this code the focus was also to test the basic yet best practice when it comes to DYNAMIC MEMORY
+ */
+
+#include <iostream>
+#include <string>
+
+using std::cout; using std::endl; using std::string;
+
