3577eb22c9cee79390a1c9dc90be2d9ff05f4db5
mooracle <yanuar.prakosa@gmail.com>
2022-09-18 19:35:41 +0700

    give final Guidelines F42 and F43

--- intermediate/intermediate-24/source/main.cpp
+++ intermediate/intermediate-24/source/main.cpp
@@ -7,55 +7,60 @@ In this code the focus was also to test the basic yet best practice when it come
 #include <iostream>
 #include <string>
 
-using std::cout; using std::endl; using std::string;
+using std::cout; using std::getline; using std::string; using std::endl;
 
-void Print_add(int* a) {
-    /* 
-    This function is to demo to pointer as paremeter.
-    This will take int pointer as argument and process it
-     */
-    // *a++; // <- since we are using pointer then this is not allowed it will increment the address not the value inside
-    *a = *a + 1;
-
-    // begin test in Print_add function:
-    cout << "\nFrom Print_add function: \n";
-    cout << "pointer address: " << &a << endl;
-    cout << "pointer target: " << a << endl;
-    cout << "pointer value: " << *a << endl;
-
-    // clear the pointer a target:
-    delete a;
-    cout << "\nend of Print_add test\n";
+int* WorstFunctionEver() {
+    int* f = new int(5);
+    return f;
 }
 
-int main() {
-    int* ib;
-    ib = new int(10);
-    Print_add(ib);
+int* Create8() {
+    int e = 8;
+    return &e;
+}
 
-    // Begin test in main function:
-    cout << "\nFrom main function: \n";
-    cout << "pointer address: " << &ib << endl;
-    cout << "pointer target: " << ib << endl;
-    cout << "pointer value: " << *ib << endl;
-    delete ib;
-    cout << "\ntest end process" << endl;
+void Set7(int* p) {
+    *p = 7;
+}
+/* 
+test using main function
+ */
+int main() {
+    int* p = Create8();
+    cout << "\ntest pointer p:\n";
+    cout << "value of p: " << *p << '\n';
+    cout << "target of p: " << p << '\n';
+    cout << "address of p: " << &p << endl;
+
+    Set7(p);
+    cout << "\ntest reinstate p:\n";
+    cout << "value of p: " << *p << '\n';
+    cout << "target of p: " << p << '\n';
+    cout << "address of p: " << &p << endl;
+
+    int* t = Create8();
+    cout << "\ntest pointer t:\n";
+    cout << "value of t: " << *t << '\n';
+    cout << "target of t: " << t << '\n';
+    cout << "address of t: " << &t << endl;
+    // delete p;
+    // delete t;
     return 0;
 }
 
 /* RESULT:
-From Print_add function: 
-pointer address: 0x7ffee489f6d8
-pointer target: 0x7fce4bc059c0
-pointer value: 11
+test pointer p:
+value of p: 0
+target of p: 0x4cefffc64
+address of p: 0x4cefffca0
 
-end of Print_add test
+test reinstate p:
+value of p: 0
+target of p: 0x4cefffc64
+address of p: 0x4cefffca0
 
-From main function: 
-pointer address: 0x7ffee489f700
-pointer target: 0x7fce4bc059c0
-pointer value: 11
-main.out(5581,0x10c3d1e00) malloc: *** error for object 0x7fce4bc059c0: pointer being freed was not allocated
-main.out(5581,0x10c3d1e00) malloc: *** set a breakpoint in malloc_error_break to debug
-[1]    5581 abort      ./main.out
+test pointer t:
+value of t: 0
+target of t: 0x4cefffc64
+address of t: 0x4cefffc98
  */
