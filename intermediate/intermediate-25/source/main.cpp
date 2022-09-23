a3f9626b7d7de2faa3b6d03130b182bea1e03df2
mooracle <yanuar.prakosa@gmail.com>
2022-09-23 08:18:20 +0700

    demo user defined dynamic array size and contents

--- intermediate/intermediate-25/source/main.cpp
+++ intermediate/intermediate-25/source/main.cpp
@@ -4,18 +4,77 @@
 using std::cout; using std::endl; using std::string; using std::cin;
 
 int main() {
-    int* p1 = new int[5]; // <- this creates new dynamic array with size of 5 int(s)
-    // int* p2 = new int[]{1,2,3}; this declaration is not allowed!
-    int* p2 = new int[6]; //<- just stick to this type of dynamic array declaration
+    int arraySize, inputNumber;
+    // begin by asking the user their intended array size:
+    cout << "Define array size: ";
+    cin >> arraySize;
 
-    // compare it to the non dynamic array declaration and definition:'
-    int a[] {1,2,3}; // <- this is allowed since it will fixed by the stack as array size 3 ints
-    int b[4]; //<- this declaration put the stack array with empty member but with size 4 ints
+    // create Dynamic Array as user's size intended:
+    int* p = new int[arraySize];
 
-    // always remember to clean up:
-    // delete p1; WRONG: 'delete' applied to a pointer that was allocated with 'new[]'; did you mean 'delete[]'? [-Wmismatched-new-delete]
-    delete[] p1;
-    delete[] p2;
-    cout << "end of run" << endl;
+    // ask user to input their numbers into the array:
+    for (int i = 0; i < arraySize; i++)
+    {
+        switch (i)
+        {
+        case 0:
+            cout << "Enter 1st number: ";
+            break;
+        case 1:
+            cout << "Enter 2nd number: ";
+            break;
+        case 2:
+            cout << "Enter 3rd number: ";
+            break;
+        default:
+            cout << "Enter " << i + 1 << "th number: ";
+            break;
+        }
+        cin >> inputNumber;
+        p[i] = inputNumber;
+    }
+
+    // test the Dynamic Array:
+    cout << "\ntest dynamic array output: \n";
+    for (int k=0; k < arraySize; k++)
+    {
+        switch (k)
+        {
+        case 0:
+            cout << "1st number: " << p[k] << '\n';
+            break;
+        case 1:
+            cout << "2nd number: " << p[k] << '\n';
+            break;
+        case 2:
+            cout << "3rd number: " << p[k] << '\n';
+            break;
+        default:
+            cout << k + 1 << "th number: " << p[k] << '\n';
+            break;
+        }
+    }
+
+    delete[] p;
+    
+    cout << "\nend of run" << endl;
     return 0;
 }
+
+/* RESULT:
+Define array size: 5
+Enter 1st number: 1
+Enter 2nd number: 2
+Enter 3rd number: 3
+Enter 4th number: 4
+Enter 5th number: 5
+
+test dynamic array output:
+1st number: 1
+2nd number: 2
+3rd number: 3
+4th number: 4
+5th number: 5
+
+end of run
+ */
